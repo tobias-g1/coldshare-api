e1b15c6ceaa76d1ff1ee1b2f8be4503e1d4ba092
tobias-g1 <tobias.gooney@icloud.com>
2023-06-02 14:54:46 +0100

    Define file models

---
+++ src/models/file.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    sharedWith: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    format: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    mimeType: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const File = mongoose.model("File", fileSchema);
+
+export { File };
---
+++ src/models/pin-code.model.js
@@ -0,0 +1,41 @@
+const pinCodeSchema = new mongoose.Schema(
+  {
+    file: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "File",
+      required: true,
+    },
+    pinCode: {
+      type: String,
+      required: true,
+      unique: true,
+      default: generateUniquePinCode,
+    },
+    expirationDate: {
+      type: Date,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+function generateUniquePinCode() {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const pinCodeLength = 6;
+  let pinCode = "";
+
+  for (let i = 0; i < pinCodeLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    pinCode += characters.charAt(randomIndex);
+  }
+
+  return pinCode;
+}
+
+const PinCode = mongoose.model("PinCode", pinCodeSchema);
+
+export { PinCode };
